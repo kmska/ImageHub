1cf6dc5bb994557abdb3d3b449c735eb73d5de53
Hero Solutions <michiel@herosolutions.be>
2019-10-02 17:13:22 +0200

    Added: iiif_ptif plugin with readme

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # ImageHub
+
+
+# Usage
+
+This project contains a ResourceSpace plugin to generate Tiled Pyramidal TIFF files when uploading a new image.
+
+In order to make use of this plugin, the iiif_ptif/ folder should be copied to the plugins/ folder of your ResourceSpace installation and activated by the system administrator (System -> Manage plugins, under 'System').
+
+The following lines should also be added to the configuration file of your ResourceSpace installation (include/config.php):
+
+```
+# 'name' and 'target_extension' must always be IIIF_PTIF,
+# these are required by the ptif_files plugin to work properly.
+$image_alternatives[0]['name']              = 'IIIFPTIF';
+$image_alternatives[0]['target_extension']  = 'IIIFPTIF';
+$image_alternatives[0]['source_extensions'] = 'jpg,png,tif,psb,psd';
+$image_alternatives[0]['source_params']     = '';
+$image_alternatives[0]['filename']          = 'IIIF_Tiled_Pyramidal_TIFF';
+$image_alternatives[0]['params']            = '-define tiff:tile-geometry=256x256 -compress jpeg';
+$image_alternatives[0]['icc']               = false;
+
+# This must be set to NULL in order to fix a bug within ResourceSpace
+# where resource files are not properly deleted if this value is set to anything other than NULL.
+# This bug resides in include/resource_functions.php:2015.
+$resource_deletion_state = NULL;
+```
---
+++ iiif_ptif/hooks/all.php
@@ -0,0 +1,40 @@
+<?php
+    # Generate Tiled Pyramidal TIFF files when uploading a new image
+    # Should be defined in config.php as such, along with any necessary command line arguments
+    # $image_alternatives[0]['name']              = 'IIIFPTIF';
+    # $image_alternatives[0]['target_extension']  = 'IIIFPTIF';
+
+    # Return the path '/filestore/iiif/$ref.tif' to store PTIF files in when uploading a new image
+    function getPtifFilePath($ref)
+    {
+        global $storagedir;
+        return $storagedir . '/' . $ref . '.tif';
+    }
+
+    # Return the path prefixed with 'ptif:' to pass to ImageMagick's convert command to generated Tile Pyramidal TIFF files
+	function HookIiif_ptifAllGet_resource_path_override($ref, $getfilepath, $size, $generate,
+		$extension, $scramble, $page, $watermarked, $file_modified, $alternative, $includemodified)
+	{
+		if($extension == 'IIIFPTIF')
+			return 'ptif:' . getPtifFilePath($ref);
+		else
+			return false;
+	}
+
+    # Filter the alternative files view to exclude the PTIF files
+    # This removes them from both the resource view page and also the 'manage alternative files' area.
+	function HookIiif_ptifAllGet_alternative_files_extra_sql($resource)
+    {
+	    return "and name <> 'IIIFPTIF'";
+    }
+
+    # Delete any generated PTIF files associated with this resource when the resource is being deleted
+    function HookIiif_ptifAllBeforedeleteresourcefromdb($ref)
+    {
+       	$path = getPtifFilePath($ref);
+		if (file_exists($path)) {
+			unlink($path);
+		}
+    }
+
+?>
