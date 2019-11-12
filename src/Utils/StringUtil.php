9a251d34956ebb0eafdd0f45b907b3537082c7c3
Hero Solutions <michiel@herosolutions.be>
2019-11-12 15:12:41 +0100

    Translates object number to data PID

--- src/Utils/StringUtil.php
+++ src/Utils/StringUtil.php
@@ -13,4 +13,50 @@ class StringUtil
         }
         return $filename;
     }
+
+    public static function cleanObjectNumber($nr)
+    {
+        // Patterns, see https://github.com/PACKED-vzw/resolver/blob/master/resolver/util.py, def cleanID
+        $patterns = array(
+            // Exceptions
+            '- '   => '-',
+            ' -'   => '-',
+            '\)+$' => '',
+            '\]+$' => '',
+            '\°+$' => '',
+            // Simple replacements
+            '\.'   => '_',
+            ' '    => '_',
+            '\('   => '_',
+            '\)'   => '_',
+            '\['   => '_',
+            '\]'   => '_',
+            '\/'   => '_',
+            '\?'   => '_',
+            ','    => '_',
+            '&'    => '_',
+            '\+'   => '_',
+            '°'    => '_',
+            // Replace two or more underscores by a single underscore
+            '__+'   => '_',
+            // Remove any underscores at the end
+            '_$'    => ''
+        );
+
+        // Keep repeating the replacement until there are no more
+        $hasPattern = true;
+        while($hasPattern) {
+            $hasPattern = false;
+            foreach ($patterns as $pattern => $replacement) {
+                if(preg_match('/^.*' . $pattern . '.*$/', $nr)) {
+                    $hasPattern = true;
+                    if(strpos($pattern, '$') < 0) {
+                        $pattern = $pattern . '(.*)$';
+                    }
+                    $nr = preg_replace('/^(.*)' . $pattern . '/', '${1}' . $replacement . '${2}', $nr);
+                }
+            }
+        }
+        return $nr;
+    }
 }
