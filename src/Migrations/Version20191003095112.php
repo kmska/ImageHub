338e9d5e08af08c4c5c5eeb30c5a7db5b59bca37
unknown <info@herosolutions.be>
2021-05-18 16:32:07 +0200

    Adds datahub_data entity

--- src/Migrations/Version20191003095112.php
+++ src/Migrations/Version20191003095112.php
@@ -23,6 +23,7 @@ final class Version20191003095112 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE iiif_manifest (id INT AUTO_INCREMENT NOT NULL, manifest_id VARCHAR(255) NOT NULL, data LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE datahub_data (id VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, value TEXT, PRIMARY KEY(id, name)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
@@ -31,5 +32,6 @@ final class Version20191003095112 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('DROP TABLE iiif_manifest');
+        $this->addSql('DROP TABLE datahub_data');
     }
 }
