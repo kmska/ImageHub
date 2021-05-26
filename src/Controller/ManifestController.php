708b6365443b4c4aae6b5d72f3e044aad0a88d46
unknown <info@herosolutions.be>
2021-05-26 20:39:40 +0200

    Support HEAD requests to return 200 or 404 without returning actual result

--- src/Controller/ManifestController.php
+++ src/Controller/ManifestController.php
@@ -18,44 +18,62 @@ class ManifestController extends AbstractController
     {
         // Make sure the service URL name ends with a trailing slash
         $baseUrl = rtrim($this->getParameter('service_url'), '/') . '/';
-        $manifest = $this->get('doctrine')->getRepository(IIIfManifest::class)->findOneBy(['manifestId' => $baseUrl . $manifestId . '/manifest.json']);
-        if($manifest == null) {
-            return new Response('Sorry, the requested document does not exist.', 404);
-        } else {
-            $authenticated = true;
-            $whitelist = $this->getParameter('authentication_whitelist');
-            $whitelisted = false;
-            if($request->getClientIp() != null) {
-                if(in_array($request->getClientIp(), $whitelist)) {
-                    $whitelisted = true;
-                    $authenticated = true;
-                }
-            }
 
-            $data = json_decode($manifest->getData(), true);
-            if(!$whitelisted) {
-                if (array_key_exists('service', $data)) {
-                    if (array_key_exists('@id', $data['service'])) {
-                        if (strpos($data['service']['@id'], 'auth') > -1) {
-                            $authenticated = false;
+        if($request->getMethod() == 'HEAD') {
+            $ids = $this->container->get('doctrine')
+                ->getManager()
+                ->createQueryBuilder()
+                ->select('m.id')
+                ->from(IIIfManifest::class, 'm')
+                ->where('m.manifestId = :id')
+                ->setParameter('id', $baseUrl . $manifestId . '/manifest.json')
+                ->getQuery()
+                ->getResult();
+            if(count($ids) > 0) {
+                return new Response('', 200);
+            } else {
+                return new Response('', 404);
+            }
+        } else {
+            $manifest = $this->get('doctrine')->getRepository(IIIfManifest::class)->findOneBy(['manifestId' => $baseUrl . $manifestId . '/manifest.json']);
+            if ($manifest == null) {
+                return new Response('Sorry, the requested document does not exist.', 404);
+            } else {
+                $authenticated = true;
+                $whitelist = $this->getParameter('authentication_whitelist');
+                $whitelisted = false;
+                if ($request->getClientIp() != null) {
+                    if (in_array($request->getClientIp(), $whitelist)) {
+                        $whitelisted = true;
+                        $authenticated = true;
+                    }
+                }
+
+                $data = json_decode($manifest->getData(), true);
+                if (!$whitelisted) {
+                    if (array_key_exists('service', $data)) {
+                        if (array_key_exists('@id', $data['service'])) {
+                            if (strpos($data['service']['@id'], 'auth') > -1) {
+                                $authenticated = false;
+                            }
                         }
                     }
                 }
-            }
-            if(!$authenticated) {
-                // Authenticate the user through the AD FS with SAML
-                if (Authenticator::authenticate($this->getParameter('adfs_requirements'))) {
-                    $authenticated = true;
+                if (!$authenticated) {
+                    // Authenticate the user through the AD FS with SAML
+                    if (Authenticator::authenticate($this->getParameter('adfs_requirements'))) {
+                        $authenticated = true;
+                    }
                 }
-            }
-            if(!$authenticated) {
-                return new Response('Sorry, you are not allowed to access this document.');
-            } else {
-                $headers = array(
-                    'Content-Type' => 'application/json',
-                    'Access-Control-Allow-Origin' => '*'
-                );
-                return new Response(json_encode($data, JSON_PRETTY_PRINT + JSON_UNESCAPED_SLASHES + JSON_UNESCAPED_UNICODE), 200, $headers);
+                if (!$authenticated) {
+                    return new Response('Sorry, you are not allowed to access this document.');
+                } else {
+                    $headers = array(
+                        'Content-Type' => 'application/json',
+                        'Access-Control-Allow-Origin' => '*'
+                    );
+                    return new Response(json_encode($data, JSON_PRETTY_PRINT + JSON_UNESCAPED_SLASHES + JSON_UNESCAPED_UNICODE), 200, $headers);
+                }
             }
         }
     }
